f2a91129c3354636c47a83444976df4a5f351825
Aleksi Vankka <alsu.96@hotmail.fi>
2019-01-28 11:50:33 +0200

    Add momentjs for lunch

--- src/services/lunch.js
+++ src/services/lunch.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
+const moment = require('moment')
 
 const getLunch = async () => {
-    const year = '2019'
-    const month = '01'
-    const day = '28'
+    const year = moment().format('Y')
+    const month = moment().format('MM')
+    const day = moment().format('DD')
     const response = await axios
         .get(
             `https://www.sodexo.fi/ruokalistat/output/daily_json/31314/${year}/${month}/${day}/fi`
